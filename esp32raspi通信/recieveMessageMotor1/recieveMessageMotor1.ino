4cced85800a788d7719198b28ed5c498742d3be9
TY-ux59 <se23959h@st.omu.ac.jp>
2024-08-27 13:12:45 +0900

    仕分けのモーターのプログラム
    ラズパイやコントローラーの命令に応じて動かす。角度については後で変更する必要がある。

---
+++ esp32raspi通信/recieveMessageMotor1/recieveMessageMotor1.ino
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <Servo.h>
+//サーボモーターを制御するためのインスタンスを作成
+void setup() {
+  Serial.begin(115200);
+  Servo servo;
+  servo.attach(9);  //サーボモーターの制御信号ピンを指定
+  const char match1 = "M1Temp";
+  const char match2 = "openConst"
+  servo.write(0);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String receivedMessage = Serial.readStringUntil('\n');
+    Serial.print("Received from PC: ");
+    Serial.println(receivedMessage);
+
+    if (strstr(receivedMessage, match1) != NULL) {
+      changeMotorAngleTemp(receivedMessage);
+    }
+    if (strstr(receivedMessage, match2) != NULL) {
+      changeMotorAngleConst(receivedMessage);
+    }
+  }
+}
+
+void changeMotorAngleTemp(receivedMessage) {
+  if (receivedMessage == "EbiM1") {
+    servo.write(30);
+    delay(250);
+    servo.write(-30);
+    delay(250);
+  } else if (receivedMessage == "YuzuM1") {
+    servo.write(60);
+    delay(250);
+    servo.write(-60);
+    delay(250);
+  } else {
+    servo.write(90);
+    delay(250);
+    servo.write(-90);
+    delay(250);
+  }
+}
+
+void changeMotorAngleConst() {
+  //
+  bool run = true;
+  if (receivedMessage == "EbiM1") {
+    servo.write(30);
+    delay(250);
+    while (run) {
+      run = returnCheck();
+    }
+    servo.write(-30);
+    delay(250);
+  } else if (receivedMessage == "YuzuM1") {
+    servo.write(60);
+    delay(250);
+        while (run) {
+      run = returnCheck();
+    }
+    servo.write(-60);
+    delay(250);
+  } else {
+    servo.write(90);
+    delay(250);
+        while (run) {
+      run = returnCheck();
+    }
+    servo.write(-90);
+    delay(250);
+  }
+}
+
+//コントローラーから戻すように指示が来ているかどうかを監視する
+bool returnCheck() {
+  if (Serial.available() > 0) {
+    String receivedMessage = Serial.readStringUntil('\n');
+    Serial.print("Received from PC: ");
+    Serial.println(receivedMessage);
+  }
+  if (receivedMessage == "ret"){
+    return false;
+  }else{
+    return true;
+  }
+}

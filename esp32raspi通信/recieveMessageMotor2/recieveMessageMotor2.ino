30143b7142a51c906b950e6588786ef57566e654
TY-ux59 <se23959h@st.omu.ac.jp>
2024-08-27 13:08:48 +0900

    箱のモーターのプログラム
    ラズパイやコントローラーから命令が来たらそれに応じて動く。
    初期状態を開けておく。

---
+++ esp32raspi通信/recieveMessageMotor2/recieveMessageMotor2.ino
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <Servo.h>
+
+void setup() {
+  Serial.begin(115200);
+  Servo servo;
+  servo.attach(9);
+  servo.write(45);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String receivedMessage = Serial.readStringUntil('\n');
+    Serial.print("Received from PC: ");
+    Serial.println(receivedMessage);
+
+    if (receivedMessage == "M2close") {
+      servo.write(-45);
+      delay(250);
+    }
+    if (receivedMessage == "M2Open") {
+      servo.write(45);
+      delay(250);
+    }
+    if (receivedMessage == "constOpen") {
+      servo.write(45);
+      delay(250);
+      run = true;
+      while (run) {
+        run = returnCheck();
+      }
+    }
+  }
+}
+
+//コントローラーから戻すように指示が来ているかどうかを監視する
+bool returnCheck() {
+  if (Serial.available() > 0) {
+    String receivedMessage = Serial.readStringUntil('\n');
+    Serial.print("Received from PC: ");
+    Serial.println(receivedMessage);
+  }
+  if (receivedMessage == "ret") {
+    return false;
+  } else {
+    return true;
+  }
+}

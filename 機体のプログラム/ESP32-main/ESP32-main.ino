eb7d60ec44f0d06b82cf4c04b52a504f55fa1590
TY-ux59 <se23959h@st.omu.ac.jp>
2024-09-09 19:53:03 +0900

    mainの変更

--- 機体のプログラム/ESP32-main/ESP32-main.ino
+++ 機体のプログラム/ESP32-main/ESP32-main.ino
@@ -89,8 +89,11 @@ void setup() {
   preMillis = millis();
 }
 void loop() {
-  PS4_control();
-Motor_stop();
+    for (int pos = 0; pos <= 180; pos += 1) {  // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    servo2.write(pos);  // tell servo to go to position in variable 'pos'
+    delay(15);            // waits 15ms for the servo to reach the position
+  }
 }
 
 
